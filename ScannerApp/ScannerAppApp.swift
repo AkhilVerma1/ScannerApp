324a999fae67777a934d236c9fc6a43d9190567c
Akhil Verma <42695861+AkhilVerma1@users.noreply.github.com>
2024-08-30 22:12:31 +0530

    Initial Commit

---
+++ ScannerApp/ScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerAppApp.swift
+//  ScannerApp
+//
+//  Created by Akhil Verma on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScannerAppTests/ScannerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerAppTests.swift
+//  ScannerAppTests
+//
+//  Created by Akhil Verma on 30/08/24.
+//
+
+import Testing
+@testable import ScannerApp
+
+struct ScannerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
